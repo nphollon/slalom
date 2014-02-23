7ea115aa2d8ef32af1b19c5217700ed502a8511c
nphollon <nhollon@thoughtworks.com>
2014-02-23 13:56:38 -0800

    Set the arity of the SlalomFunction using IRModuleWriter

--- src/module_writer.cpp
+++ src/module_writer.cpp
@@ -1,19 +1,49 @@
 #include "module_writer.hpp"
 
+#include <vector>
+
 #include "llvm/IR/IRBuilder.h"
 
 IRModuleWriter::IRModuleWriter(llvm::Module *m, llvm::LLVMContext *c) {
   module = m;
   context = c;
 
+  // Create SlalomFunction struct type
   llvm::Type* arityTy = llvm::Type::getInt32Ty(*context);
-  llvm::Type* retTy = llvm::StructType::create("sf", arityTy, NULL);
+  llvm::Type* sfTy = llvm::StructType::create("sf", arityTy, NULL);
+  llvm::Type* retTy = llvm::PointerType::get(sfTy, 0);
+
+  // Declare malloc
+  llvm::Type* intPtrTy = llvm::Type::getInt8PtrTy(*context);
+  llvm::Type* allocTy = llvm::Type::getInt64Ty(*context);
+  llvm::Constant* mallocC = module->getOrInsertFunction("malloc", intPtrTy, allocTy, NULL);
+  llvm::Function* mallocF = llvm::cast<llvm::Function>(mallocC);
+
+  // Create createICombinator() function
   llvm::Constant* f = module->getOrInsertFunction("createICombinator", retTy, NULL);
   llvm::Function* cic = llvm::cast<llvm::Function>(f);
+
+  // Create IRBuilder for the function
   llvm::BasicBlock* block = llvm::BasicBlock::Create(*context, "entry", cic);
   llvm::IRBuilder<> builder(block);
-  llvm::Constant* retVal = llvm::ConstantInt::get(retTy, 1);
+  
+  // Allocate memory for a struct using malloc
+  llvm::Value* allocSize = llvm::ConstantExpr::getSizeOf(sfTy);
+  llvm::Value* mallocResult = builder.CreateCall(mallocF, allocSize);
+  llvm::Value* retVal = builder.CreateBitCast(mallocResult, retTy);
+  
+  // Get a pointer to the arity element
+  llvm::Value* idxZero = llvm::ConstantInt::get(llvm::Type::getInt32Ty(*context), 0);
+  std::vector<llvm::Value*> idxList(2, idxZero);
+  llvm::Value* arityPtr = builder.CreateGEP(retVal, idxList);
+
+  // Set the arity to 1
+  llvm::Value* arity1 = llvm::ConstantInt::get(arityTy, 1);
+  builder.CreateStore(arity1, arityPtr);
+  
+  // Return pointer to the struct
   builder.CreateRet(retVal);
+
   module->dump();
 }
 
--- test/module_test.cpp
+++ test/module_test.cpp
@@ -20,8 +20,9 @@ BOOST_FIXTURE_TEST_SUITE( test_module_writer, ModuleFixture )
 
 BOOST_AUTO_TEST_CASE( i_comb_arity_is_1 ) {
   FactoryFunction createI = jit->getFunctionPointer("createICombinator");
-  //  SlalomFunctionStruct *sfs = createI();
-  //BOOST_CHECK_EQUAL(sfs->arity, 1);
+  SlalomFunctionStruct *sfs = createI();
+  BOOST_CHECK_EQUAL(sfs->arity, 1);
+  delete sfs;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
